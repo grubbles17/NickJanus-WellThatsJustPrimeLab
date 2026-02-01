d3cc33d08cdc09048a79551e0c0b6cab23848d37
grubbles17 <46872313+grubbles17@users.noreply.github.com>
2026-02-01 13:20:07 -0600

    Implement user input for prime factorization

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,26 @@
 package org.example;
+import java.util.List;
+import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
     // Test your Factorizer class using a driver program here!
+    Factorizer factorizer = new Factorizer();
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter a number >= 2: ");
+    if(!scanner.hasNextInt()){
+      System.out.println("Invalid, Please enter the proper number...");
+      return;  
+    }
+
+    int number = scanner.nextInt();
+    try{
+      List<Integer> factors = factorizer.getPrimeFactors(number);
+      System.out.println("Prime factors of " + number + " : " + factors);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: " + e.getMessage());
+    }
+    scanner.close();
   }
 }
