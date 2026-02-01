0540feaa9380b2ebb88ab2d3d330a3ea977f6a27
grubbles17 <46872313+grubbles17@users.noreply.github.com>
2026-02-01 13:21:01 -0600

    Update FactorizerTest to use getPrimeFactors method

--- app/src/test/java/org/example/FactorizerTest.java
+++ app/src/test/java/org/example/FactorizerTest.java
@@ -2,12 +2,29 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.beans.Transient;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class FactorizerTest {
   @Test
   void itReturnsAnEmptyListForZero() {
     Factorizer factorizer = new Factorizer();
-    assertEquals(factorizer.primeFactors(0), new ArrayList<Integer>());
+    assertEquals(factorizer.getPrimeFactors(0), new ArrayList<Integer>());
   }
+
+  @Test
+  void testPrimeNumbers(){
+    Factorizer f = new Factorizer();
+    assertEquals(Arrays.asList(15), f.getPrimeFactors(15));
+  }
+
+  @Test
+  void testHundred(){
+    Factorizer f = new Factorizer();
+    assertEquals(Arrays.asList(2,2,5,5), f.getPrimeFactors(100));
+  }
+
 }
+
