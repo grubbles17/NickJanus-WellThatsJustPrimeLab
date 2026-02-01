57803d9a4857d34634a42a20d4b31a47f33d21ed
grubbles17 <46872313+grubbles17@users.noreply.github.com>
2026-02-01 13:36:41 -0600

    Refactor FactorizerTest with additional test cases

--- app/src/test/java/org/example/FactorizerTest.java
+++ app/src/test/java/org/example/FactorizerTest.java
@@ -1,30 +1,30 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.beans.Transient;
-import java.util.ArrayList;
 import java.util.Arrays;
 
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class FactorizerTest {
-  @Test
-  void itReturnsAnEmptyListForZero() {
-    Factorizer factorizer = new Factorizer();
-    assertEquals(factorizer.getPrimeFactors(0), new ArrayList<Integer>());
-  }
 
-  @Test
-  void testPrimeNumbers(){
-    Factorizer f = new Factorizer();
-    assertEquals(Arrays.asList(15), f.getPrimeFactors(15));
-  }
+    @Test
+    void testOdd() {
+        Factorizer f = new Factorizer();
+        assertEquals(Arrays.asList(3,5), f.getPrimeFactors(15));
+    }
+   
+    @Test
+    void testEven() {
+        Factorizer f = new Factorizer();
+        assertEquals(Arrays.asList(2,2,5), f.getPrimeFactors(20));
+    }
 
-  @Test
-  void testHundred(){
-    Factorizer f = new Factorizer();
-    assertEquals(Arrays.asList(2,2,5,5), f.getPrimeFactors(100));
-  }
-
+    @Test
+    void testHundred() {
+        Factorizer f = new Factorizer();
+        assertEquals(Arrays.asList(2, 2, 5, 5), f.getPrimeFactors(100));
+    }   
 }
 
